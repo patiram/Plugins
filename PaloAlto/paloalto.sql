4bbd40202f1d70c0266087ab86362616654d64b3
Alex Lisle <alisle@alienvault.com>
2012-06-14 16:26:15 +0100

    Palo Alto 2500 / 5500 Plugin Added based on the Logs found in the Logs Repo

---
+++ PaloAlto/paloalto.sql
@@ -0,0 +1,5 @@
+INSERT INTO plugin_sid(plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1615, 30, NULL, NULL, "PaloAlto: INFO event",  1, 2);
+INSERT INTO plugin_sid(plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1615, 31, NULL, NULL, "PaloAlto: WARNING event",  1, 2);
+INSERT INTO plugin_sid(plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1615, 32, NULL, NULL, "PaloAlto: ERROR event",  2, 2);
+INSERT INTO plugin_sid(plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1615, 33, NULL, NULL, "PaloAlto: CRITICAL event",  3, 2);
+

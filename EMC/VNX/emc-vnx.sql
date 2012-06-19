20e5206b463ec70736f05bf30161cafda0dccb25
Alex Lisle <alisle@alienvault.com>
2012-06-19 10:23:00 +0100

    Very Rough EMC VNX Plugin added, it's use is extremely limited as it's pretty useless to even bother parsing it's ridiculous logs.

---
+++ EMC/VNX/emc-vnx.sql
@@ -0,0 +1,4 @@
+INSERT INTO plugin(id, type, name, description, source_type, vendor) VALUES (93002, 1, 'EMC-VNX', 'EMC VNX', 'SANS', 'EMC');
+INSERT INTO plugin_sid(plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (93002, 1, NULL, NULL, "VNX Process Ran",  1, 2);
+
+

d8c119dede2678ad46f2813f80b6c603c3524f89
Alex Lisle <alisle@alienvault.com>
2012-07-10 11:35:22 +0100

    Updated Tripwrite and Added a simple ARA Network Plugin

---
+++ ARA_Networks/ARANetworks.sql
@@ -0,0 +1,3 @@
+
+INSERT IGNORE INTO plugin (id, type, name, description) VALUES (9123, 1, 'ARA Networks', 'ARA Networks Plugin');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9123, 0, NULL, NULL, 'ARA Networks: Login Event' , 2, 2);

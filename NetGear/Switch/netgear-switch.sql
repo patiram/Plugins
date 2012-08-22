e40a20201551e992b49c5980154f8c6bfc6a3ce9
Alex Lisle <alisle@alienvault.com>
2012-08-22 11:07:20 +0100

    Added NetGear Plugin, which is a clone of the Cisco log format

---
+++ NetGear/Switch/netgear-switch.sql
@@ -0,0 +1,3 @@
+
+INSERT IGNORE INTO plugin (id, type, name, description) VALUES (9917, 1, 'NetGear Switch', 'NetGear');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9917, 1, NULL, NULL, 'NetGear Message USERDATA9' , 2, 2);

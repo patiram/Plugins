0376c4e58fbe5e19cd51996cada50263d68ca08f
Alex Lisle <alisle@alienvault.com>
2012-08-23 10:59:25 +0100

    Added Aruba Wireless, Changed SonicWall

---
+++ Aruba/aruba.sql
@@ -0,0 +1,5 @@
+
+INSERT IGNORE INTO plugin (id, type, name, description) VALUES (9918, 1, 'Aruba Wireless', 'Aruba');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9918, 1, NULL, NULL, 'Aruba Message USERDATA9' , 3, 2);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9918, 2, NULL, NULL, 'Aruba Message USERDATA9' , 2, 2);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9918, 3, NULL, NULL, 'Aruba Message USERDATA9' , 1, 2);

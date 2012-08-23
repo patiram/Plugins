112c35251c153f95f5482d1f363cb17f8cbe723c
Alex Lisle <alisle@alienvault.com>
2012-08-23 14:31:42 +0100

    Added Barracuda WebFilter

---
+++ Barracuda/WebFilter/barracuda-webfilter.sql
@@ -0,0 +1,5 @@
+
+INSERT IGNORE INTO plugin (id, type, name, description) VALUES (9919, 1, 'Barracuda WebFilter', 'WebFilter');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9919, 3, NULL, NULL, 'WebFilter USERDATA2 USERDATA3 USERDATA7' , 3, 2);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9919, 2, NULL, NULL, 'WebFilter USERDATA2 USERDATA3 USERDATA7' , 2, 2);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9919, 1, NULL, NULL, 'WebFilter USERDATA2 USERDATA3 USERDATA7' , 1, 2);

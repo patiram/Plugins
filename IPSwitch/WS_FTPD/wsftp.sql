7ca821e43e997a2e052985d34f35a394c7354f84
Alex Lisle <alisle@alienvault.com>
2012-09-06 10:00:54 -0700

    Added WS_FTPD

---
+++ IPSwitch/WS_FTPD/wsftp.sql
@@ -0,0 +1,3 @@
+
+INSERT IGNORE INTO plugin (id, type, name, description) VALUES (9920, 1, 'IPSwitch WS_FTPD', 'WS_FTPD');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9920, 1, NULL, NULL, 'WS_FTPD: USERDATA1' , 2, 2);
